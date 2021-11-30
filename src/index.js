e7b62cec4e54a7216a15ce4f0a90d413693e4a48
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-11-29 19:05:50 -0500

    inicializacion del proyecto de graphql usando apollo-server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './graphql/types.js';
+import { resolvers } from './graphql/resolvers.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen({ port: 4000 }, () => {
+const server = new ApolloServer({ typeDefs, resolvers });
+
+app.listen({ port: 4000 }, async () => {
   console.log('Server on port 4000');
+  await server.start();
+  server.applyMiddleware({ app });
 });
