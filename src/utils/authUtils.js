9588136b49632099a4c2e996f439222e6811ee00
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-12-02 20:17:05 -0500

    agregada funcion para comprar contraseñas encriptadas

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -4,3 +4,7 @@ export const encryptPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
+
+export const validatePassword = async (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
